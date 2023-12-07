c5de3732220802d4f40942af33d08a8d10eba9cb
Tony Anderson <anderson.tony.98@gmail.com>
2023-12-07 20:36:19 -0300

    Final

--- ContaBanco/src/ContaTermianal.java
+++ ContaBanco/src/ContaTermianal.java
@@ -20,20 +20,32 @@ public class ContaTermianal {
 
         // Agência
         System.out.print("Agência: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Por favor, insira um número de agência válido");
+            scanner.next(); // Consome a entrada inválida
+        }
         agencia = scanner.nextInt();
 
         // Conta
         System.out.print("Conta: ");
+        while(!scanner.hasNextInt()) {
+            System.out.println("Por favor, insira um número de conta válido");
+            scanner.next(); // Consome a entrada inválida
+        }
         conta = scanner.nextInt();
-
+        
         // Saldo
-        System.out.print("Insira o seu saldo dos sonhos");
+        System.out.print("Insira o seu saldo dos sonhos: ");
+        while(!scanner.hasNextFloat()) {
+            System.out.println("Insira um saldo válido");
+            scanner.next(); // Consome a entrada inválida
+        }
         saldo = scanner.nextFloat();
 
 
         // Mensagem final ao usuário
         System.out.println("\nOlá, " + nome + ", obrigado por criar uma conta em nosso banco");
-        System.out.println("Sua agência é " + agencia + ", sua conta é " + conta + ".");
+        System.out.println("Sua agência é " + agencia + ", sua conta é " + conta);
         System.out.println("O seu saldo é: " + saldo + "\n");
         }
 }
