d7ad9954712a5fd8ab9a71ef70412fc1bdf2035b
Tony Anderson <anderson.tony.98@gmail.com>
2023-12-07 20:23:11 -0300

    versao sem verificacoes

--- ContaBanco/src/ContaTermianal.java
+++ ContaBanco/src/ContaTermianal.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class ContaTermianal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        String nome;
+        Integer agencia;
+        Integer conta;
+        Float saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Mensagem inicial
+        System.out.println("---------------------------------------");
+        System.out.println("  Bem vindo ao nosso sistema bancário  ");
+        System.out.println("---------------------------------------\n");
+
+        // Nome
+        System.out.print("Insira seu nome: ");
+        nome = scanner.nextLine();
+
+        // Agência
+        System.out.print("Agência: ");
+        agencia = scanner.nextInt();
+
+        // Conta
+        System.out.print("Conta: ");
+        conta = scanner.nextInt();
+
+        // Saldo
+        System.out.print("Insira o seu saldo dos sonhos");
+        saldo = scanner.nextFloat();
+
+
+        // Mensagem final ao usuário
+        System.out.println("\nOlá, " + nome + ", obrigado por criar uma conta em nosso banco");
+        System.out.println("Sua agência é " + agencia + ", sua conta é " + conta + ".");
+        System.out.println("O seu saldo é: " + saldo + "\n");
+        }
 }
